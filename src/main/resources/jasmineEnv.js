9f485a1f6e1bb7b9a83360fe3fd57897732ac2f1
Tomasz Dziurko <tdziurko@gmail.com>
2013-03-19 13:27:22 +0100

    TeamCity reporter moved to separate directory

--- src/main/resources/jasmineEnv.js
+++ src/main/resources/jasmineEnv.js
@@ -6,4 +6,4 @@ EnvJasmine.loadLibGlobal("envjs/env.rhino.1.2.js");
 EnvJasmine.loadLibGlobal("jasmine/jasmine.js");
 EnvJasmine.loadLibGlobal("jasmine-jquery/jasmine-jquery-1.2.0.js");
 EnvJasmine.loadLibGlobal("jasmine-rhino-reporter/jasmine-rhino-reporter.js");
-EnvJasmine.loadLibGlobal("jasmine-teamcity-reporter.js");
+EnvJasmine.loadLibGlobal("jasmine-teamcity-reporter/jasmine-teamcity-reporter.js");
